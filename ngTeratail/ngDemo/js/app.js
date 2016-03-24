a5ca7988c4c2995242eea7a4fc5c0c11407291e1
蓮子 和也 <hatsushi_kazuya@liginc.co.jp>
2016-03-24 11:15:04 +0900

    ES6に変更

--- ngTeratail/ngDemo/js/app.js
+++ ngTeratail/ngDemo/js/app.js
@@ -2,20 +2,20 @@
 	angular.module('myApp', [])
 		.component('todo', {
 			controllerAs: 'app',
-			controller: function(){
+			controller: function() {
 				this.list = [];
 				this.clickButton = () => {
 					this.drawLine();
 				};
-				this.drawLine = function() {
+				this.drawLine = ()  => {
 					this.todo.date = Date.now();
 					this.list.push(this.todo);
 					this.todo = {};
 				};
-				this.clickList = function(item){
+				this.clickList = (item) => {
 					item.done = !item.done;
 				};
-				this.clickDeleteButton = function(item){
+				this.clickDeleteButton = (item) => {
 					var idx = this.list.indexOf(item);
 					if(idx >=0){
 						this.list.splice(idx, 1);
